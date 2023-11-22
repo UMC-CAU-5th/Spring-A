9b1841d271cb93743702ff02f866b91947310af3
HanSeongLEE <leehs98712@gmail.com>
2023-11-22 19:21:18 +0900

    [feat] 8주차 구현 완료

---
+++ 올리브/UMC_SERVER_WORKBOOK/src/main/java/umc/study/web/dto/MissionRequestDTO.java
@@ -0,0 +1,19 @@
+package umc.study.web.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MissionRequestDTO {
+
+//    @Builder
+//    @Getter
+//    @NoArgsConstructor
+//    @AllArgsConstructor
+//    public static class JoinResultDTO{
+//        Long memberId;
+//        LocalDateTime createdAt;
+//    }
+}
